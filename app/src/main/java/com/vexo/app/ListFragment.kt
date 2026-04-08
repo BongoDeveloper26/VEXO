0ced96231d62fa4dc6516190032702c95d6fe7aa
Viarium <gestion.interna@viarium.es>
2026-04-08 12:22:46 +0200

    21º actualización

--- app/src/main/java/com/vexo/app/ListFragment.kt
+++ app/src/main/java/com/vexo/app/ListFragment.kt
@@ -6,14 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.tabs.TabLayout
